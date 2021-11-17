c8a01218fe807ab1caf58aeff743d362b92cfd4e
carolina <dennysgovea@gmail.com>
2021-11-17 01:52:04 -0300

    creacion del nav

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './style.css';
+
+const NavBar = () => {
+    return (
+        <nav className="containerNav">
+            <div className="divlogo">
+                <h1 className="logo"> <a className="link" href="/#"> Store</a></h1>
+            </div>
+            <ul className="containerList">
+                <li className= "item"> <a className="link" href="/#"> Inicio</a></li>
+                <li className= "item"> <a className="link" href="/#"> Productos</a></li>
+                <li className= "item"> <a className="link" href="/#"> Categorías</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
+
+
+
