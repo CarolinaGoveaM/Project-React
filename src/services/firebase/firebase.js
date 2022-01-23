025bb73596f5034bb628f26254d02049c96725a7
carolina <dennysgovea@gmail.com>
2022-01-22 22:21:24 -0300

    agregado de .env

--- src/services/firebase/firebase.js
+++ src/services/firebase/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAGkE5T-OWImM5sZJWIbqOxv2KKFuieyc8",
-    authDomain: "app-react-ef0e1.firebaseapp.com",
-    projectId: "app-react-ef0e1",
-    storageBucket: "app-react-ef0e1.appspot.com",
-    messagingSenderId: "1014819336494",
-    appId: "1:1014819336494:web:70a044ac85b204284b8abf"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
   };
 
   const app = initializeApp(firebaseConfig);
